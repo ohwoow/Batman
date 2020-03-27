3f35b0e1636146f44a935d864a7f18e4217433b0
Даня <diman100l@mail.ru>
2020-03-27 12:59:33 +0500

    Final

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    let menuLink = $('.full-info__menu-link');
+    
+    menuLink.on('click', function(event){
+        event.preventDefault();
+        let activeContent = $(this).attr('href');
+        $('.visible').toggleClass('visible');
+        $(activeContent).toggleClass('visible');
+        $('.full-info__menu-link--active').toggleClass('full-info__menu-link--active')
+        $(this).toggleClass('full-info__menu-link--active');
+    });
+});
